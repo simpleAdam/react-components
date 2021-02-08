442181c686236cc1cdd72a122b96b655ec36598a
simpleAdam <34377387+simpleAdam@users.noreply.github.com>
2021-02-08 22:09:10 +0000

    Update Col.js

--- Col.js
+++ Col.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const Col = ({children}) => (<div style={{display:'flex',flexDirection:'column'}} >{children}</div>)
+const Col = ({children}) => (<div style={{display:'flex',flexDirection:'column',width:'100%'}} >{children}</div>)
 
 export default Col
