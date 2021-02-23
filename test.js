e1152bf81e015becd827d0982933fe4e56fc6b69
simpleAdam <34377387+simpleAdam@users.noreply.github.com>
2021-02-23 20:31:51 +0000

    Update test.js
    I added a few test components

--- test.js
+++ test.js
@@ -1,5 +1,19 @@
 import React from 'react'
 
 const Row = ({children}) => (<div style={{display:'flex'}} >{children}</div>)
-
-export default Row
+                             
+const Flex = (props) => {
+  return (<div style={{display:"flex"}}>{props.children}</div>)
+}
+const FlexCol = (props) => {
+  return (<div style={{display:"flex",flexDirection:"column",margin:"30px"}}>{props.children}</div>)
+}
+const Around = (props) => {
+  return (<div style={{width:"100%",display:"flex",justifyContent:"space-around",border:"1px solid grey"}}>{props.children}</div>)
+}
+const Between = (props) => {
+  return (<div style={{width:"100%",display:"flex",justifyContent:"space-between",border:"1px solid grey"}}>{props.children}</div>)
+}
+const Center = (props) => {
+  return (<div style={{width:"100%",display:"flex",justifyContent:"center",border:"1px solid grey"}}>{props.children}</div>)
+}
